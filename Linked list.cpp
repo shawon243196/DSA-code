222988e99730dd7fcdf82ae33553d193f6ced4aa
shawon243196 <sadmanshawon542@gmail.com>
2025-09-07 20:28:16 +0600

    Update Linked list.cpp

--- Linked list.cpp
+++ Linked list.cpp
@@ -54,3 +54,71 @@ print();
 
     return 0;
 }
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+struct Node
+{
+   int data;
+   Node* link;
+};
+
+Node* head = nullptr; // Initialize head to nullptr
+
+void insert(int val)
+{
+   if (head == nullptr) // If list is empty
+   {
+      head = new Node;
+      head->data = val;
+      head->link = nullptr;
+   }
+   else // If list has at least one node
+   {
+      Node* ptr = head;
+      while(ptr->link != nullptr) // Traverse to the last node
+      {
+         ptr = ptr->link;
+      }
+      Node* nn = new Node;
+      nn->data = val;
+      nn->link = nullptr;
+      ptr->link = nn;
+   }
+}
+void display()
+{
+   if (head == nullptr) // Check if list is empty
+   {
+      cout << "List is empty" << endl;
+      return;
+   }
+   
+   Node* ptr = head;
+   while(ptr != nullptr) // Traverse until the end
+   {
+      cout << ptr->data << " ";
+      ptr = ptr->link;
+   }
+   cout << endl;
+}
+
+int main()
+{
+int n,a;
+cin>>n;
+for(int i=0;i<n;i++)
+{
+   cin>>a;
+   insert(a);
+}
+display();
+  
+
+    return 0;
+}
