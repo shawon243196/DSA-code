8ffe1c428a2a5d26a2fbcf8820fd8c1618c61025
Mohammad sadman shawon <sadmanshawon542@gmail.com>
2025-11-10 21:15:49 +0600

    Create Stack in linklist.cpp

---
+++ Stack in linklist.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Stack
+{
+   struct Node
+   {
+    int data;
+    Node*next;
+   };
+   Node*head=nullptr;
+   int size=0;
+   public:
+   void push(int val)
+   {
+    size++;
+    Node*nn=new Node;
+    nn->data=val;
+    nn->next=head;
+    head=nn;
+
+   }
+   void pop()
+   {
+    if(head==nullptr)
+    {
+        cout<<"anderflow"<<endl;
+        return;
+    }
+    Node*temp=head;
+    head=head->next;
+    delete temp;
+   }
+   int SIZE()
+   {
+    return size;
+   }
+   void display()
+   {
+    Node*ptr=head;
+    while(ptr!=nullptr)
+    {
+       
+        cout<<ptr->data<<endl;
+         ptr=ptr->next;
+    }
+   }
+};
+
+int main()
+ {
+    Stack s1,s2;
+    s1.push(10);
+    s1.push(20);
+    s1.push(100);
+    s1.display();
+    s1.pop();
+    s1.pop();
+    s1.pop();
+    cout<<endl;
+    s1.display();
+
+
+
+}
